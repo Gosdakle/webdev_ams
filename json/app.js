17ff9ac8d649985fe83481cd5e1cf8b609b3bc51
Anar <anar8radnaa@gmail.com>
2023-02-12 04:48:45 +0800

    connecting to postgresql

--- json/app.js
+++ json/app.js
@@ -1,3 +1,13 @@
+const { Client } = require('pg');
+const client = new Client({
+    host: 'localhost',
+    database: 'article',
+    user: 'postgres',
+    password: 'password',
+});
+
+client.connect();
+
 const express = require("express");
 const app = express();
 
@@ -6,21 +16,32 @@ app.use(express.json());
 const port = 3000;
 
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+    console.log(`Server is running at http://localhost:${port}`);
 });
 
 /**
  * @swagger
  * /articles:
  *   get:
+ *     tags:
+ *       - Articles
+ *     summary: Get articles
  *     description: Returns a list of articles
  *     responses:
  *       200:
  *         description: A list of articles
  */
-app.get("/articles", (req, res) => {
-    res.send("Articles GET request");
-  });
+app.get("/api/articles", (req, res) => {
+    const query = 'SELECT * FROM article_international';
+
+    client.query(query, (error, result) => {
+        if (error) {
+            res.status(500).json({ error });
+        } else {
+            res.status(200).json(result.rows);
+        }
+    });
+});
 
 const swagger = require("./swagger");
 
